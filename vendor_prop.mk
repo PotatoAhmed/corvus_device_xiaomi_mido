f377ac15e1e6fcfabaca3efa0fd26017cd8a1ba6
PotatoAhmed <ahmedbasha243@gmail.com>
2021-05-18 00:55:37 +0000

    Revert "mido: Added Dirac values in vendor.prop"
    This reverts commit 5e1d5fe3529cff87f844cad10cb49490d95d798b.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -79,15 +79,6 @@ persist.vendor.cne.feature=1
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.debug.coresight.config=stm-events
 
-# Dirac
-PRODUCT_PROPERTY_OVERRIDES += \
-hpx_send_params=1 \
-ro.audio.soundfx.dirac=true \
-persist.audio.dirac.speaker=true \
-persist.dirac.acs.controller=qem \
-persist.dirac.acs.storeSettings=1 \
-persist.dirac.acs.ignore_error=1
-
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.egl.hw=0 \
