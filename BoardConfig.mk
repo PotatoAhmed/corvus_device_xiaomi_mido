40836eebc5b0d1fea6c8e69595c963601e28b1d9
Ahmed Elwakil <35235801+PotatoAhmed@users.noreply.github.com>
2021-05-21 11:38:45 +0200

    Drop: Ignore neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,6 @@ VENDOR_SECURITY_PATCH := 2020-11-05
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
